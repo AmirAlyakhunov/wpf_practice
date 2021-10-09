65af638b3fa0c3f6767132ba0baff45f71d8852f
Amir <67748620+AmirAlyakhunov@users.noreply.github.com>
2021-10-09 15:44:41 +0300

    added 4 task

---
+++ WPF_Windows/Var2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Var2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap bitmap;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /* OpenFileDialog fileDialog = new OpenFileDialog();
+
+             if ((bool)fileDialog.ShowDialog())
+             {
+                 using (StreamReader fileStream = new StreamReader(fileDialog.FileName))
+                 {
+                     rtbText.AppendText(fileStream.ReadToEnd());
+                 }
+             }*/
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if ((bool)fileDialog.ShowDialog())
+            {
+                Background = new ImageBrush(new BitmapImage(new Uri(fileDialog.FileName)));
+                bitmap = new Bitmap(fileDialog.FileName);
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            /* SaveFileDialog saveFileDialog = new SaveFileDialog();
+             if ((bool)saveFileDialog.ShowDialog())
+             {
+                 using (StreamWriter fileStream = new StreamWriter(saveFileDialog.FileName))
+                 {
+                     TextRange doc = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+                     fileStream.Write(doc.Text);
+                 }
+             }*/
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if ((bool)saveFileDialog.ShowDialog())
+            {
+                bitmap.Save(saveFileDialog.FileName);
+            }
+        }
+    }
+}
---
+++ WPF_Windows/WindowInteracting/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowInteracting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NewWindow _window = new NewWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Стартуем окна как немодальные.
+            NewWindow w1 = new NewWindow();
+            w1.Show();
+            Window2 w2 = new Window2();
+            w2.Show();
+
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Отображаем второе окно как немодальное.
+            _window.Show();
+            // Делаем первую кнопку не активной.
+            buttonShow.IsEnabled = false;
+            // Вторую кнопку, для обновления дочернего окна, делаем активной.
+            buttonUpdate.IsEnabled = true;
+        }
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            // Вызываем пользовательский метод, который обновляет значения Label в дочернем окне.
+            _window.UpdateWindow("Hello world dadada");
+            buttonUpdate.IsEnabled = false;
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Перебираем все окна текущего приложения.
+            for (int i = 0; i < Application.Current.Windows.Count; ++i)
+            {
+                Window temp = Application.Current.Windows[i];
+                // Если окно производное от интерфейса IInteractiveWindow вызываем метод UpdateWindow().
+                if (temp is IInteractiveWindow)
+                {
+                    (temp as IInteractiveWindow).UpdateWindow("Hello world");
+                }
+            }
+        }
+
+
+    }
+}
